fe2b2fdefb9e196a67384e4d148b916a8128d5ae
Orjan Bernt Lundberg <orjan.lundberg@gmail.com>
2014-10-18 17:33:46 +0200

    Example

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -4,19 +4,34 @@ object Tables extends{ // or just use object demo.Tables, which is hard-wired to
 import Tables._
 import Tables.profile.simple._
 
-object Example extends App {
+object SlickHandsOn {
   // connection info for a pre-populated throw-away, in-memory db for this demo, which is freshly initialized on every run
   val url = "jdbc:h2:mem:test;INIT=runscript from 'src/main/sql/create.sql'"
   val db = Database.forURL(url, driver = "org.h2.Driver")
 
-  // Using generated code. Our Build.sbt makes sure they are generated before compilation.
-  val q = Companies.join(Computers).on(_.id === _.manufacturerId)
-                   .map{ case (co,cp) => (co.name, cp.name) }
+  def computersByCompany(implicit session: Session) = {
+    // Using generated code. Our Build.sbt makes sure they are generated before compilation.
+    val q = Companies.join(Computers).on(_.id === _.manufacturerId)
+      .map { case (co, cp) => (co.name, cp.name)}
 
-  db.withTransaction { implicit session =>
-    println( q.run.groupBy{ case (co,cp) => co }
-                  .mapValues(_.map{ case (co,cp) => cp })
-                  .mkString("\n")
-    )
+    q.run.groupBy { case (co, cp) => co}
+      .mapValues(_.map { case (co, cp) => cp})
+      .mkString("\n")
+  }
+
+  def numAppleComputers(implicit session: Session) = {
+    2
+  }
+
+  def main(args: Array[String]) = {
+    println("Select running option:")
+    db.withTransaction { implicit session =>
+      val result = readInt() match {
+        case 1 => computersByCompany
+	case 2 => numAppleComputers
+        case _ => "Invalid option"
+      }
+      println(result)
+    }
   }
 }
